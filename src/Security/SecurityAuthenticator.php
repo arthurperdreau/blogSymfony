5e8b247154ff53887e0fba7ce54c8322cc6d9fe8
perdreauarthur <arthur.perdreau@outlook.fr>
2025-04-21 23:34:26 +0200

    add file

--- src/Security/SecurityAuthenticator.php
+++ src/Security/SecurityAuthenticator.php
@@ -36,7 +36,7 @@ class SecurityAuthenticator extends AbstractLoginFormAuthenticator
             new UserBadge($email),
             new PasswordCredentials($request->getPayload()->getString('password')),
             [
-                //new CsrfTokenBadge('authenticate', $request->getPayload()->getString('_csrf_token')),
+                new CsrfTokenBadge('authenticate', $request->getPayload()->getString('_csrf_token')),
                 new RememberMeBadge(),
             ]
         );
