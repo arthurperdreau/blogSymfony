4e5cf30c548a52f654d4e3ba47c82bff1c33440c
perdreauarthur <arthur.perdreau@outlook.fr>
2025-04-21 22:56:20 +0200

    add file

---
+++ src/Controller/PostController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Entity\Image;
+use App\Entity\Post;
+use App\Form\CommentType;
+use App\Form\ImageType;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PostController extends AbstractController
+{
+    #[Route('/', name: 'posts')]
+    public function index(PostRepository $postRepository): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'controller_name' => 'postController',
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/post/show/{id}', name: 'show_post', priority: -1)]
+    public function show(Post $post, Request $request, EntityManagerInterface $manager): Response
+    {
+        $comment=new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setPost($post);
+            $comment->setCreatedAt(new \DateTimeImmutable());
+            $manager->persist($comment);
+            $manager->flush();
+            return $this->redirectToRoute('show_post', ['id' => $post->getId()]);
+        }
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/post/create', name: 'create_post')]
+    public function create(Request $request, EntityManagerInterface $manager): Response
+    {
+        if ($this->getUser()===null) {
+            return $this->redirectToRoute('app_login');
+        }
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setCreatedAt(new \DateTimeImmutable());
+            $post->setAuthor($this->getUser());
+            $manager->persist($post);
+            $manager->flush();
+            return $this->redirectToRoute('posts');
+        }
+        return $this->render('post/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/post/edit/{id}', name: 'edit_post')]
+    public function edit(Post $post, Request $request, EntityManagerInterface $manager): Response
+    {
+        if ($this->getUser()===null) {
+            return $this->redirectToRoute('app_login');
+        }
+        if ($post->getAuthor()!==$this->getUser()) {
+            return $this->redirectToRoute('posts');
+        }
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->flush();
+            return $this->redirectToRoute('posts');
+        }
+        return $this->render('post/edit.html.twig', [
+            'form' => $form->createView(),
+            'post' => $post,
+        ]);
+    }
+
+    #[Route('/post/delete/{id}', name: 'delete_post')]
+    public function delete(Post $post, EntityManagerInterface $manager): Response
+    {
+        if ($this->getUser()===null) {
+            return $this->redirectToRoute('login');
+        }
+        if($post){
+            $manager->remove($post);
+            $manager->flush();
+        }
+        if ($post->getAuthor()!==$this->getUser()) {
+            return $this->redirectToRoute('posts');
+        }
+
+        return $this->redirectToRoute('posts');
+    }
+
+    #[Route('/post/addimage/{id}', name: 'image')]
+    public function addImage(Post $post, Request $request, EntityManagerInterface $manager): Response
+    {
+        if ($this->getUser()===null) {
+            return $this->redirectToRoute('login');
+        }
+        if ($post->getAuthor()!==$this->getUser()) {
+            return $this->redirectToRoute('posts');
+        }
+        $image=new Image();
+        $form = $this->createForm(ImageType::class, $image);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $image->setPost($post);
+            $manager->persist($image);
+            $manager->flush();
+            return $this->redirectToRoute('posts');
+        }
+        return $this->render('post/addImage.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
